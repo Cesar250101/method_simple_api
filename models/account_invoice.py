9af2a8f112852582fc4c6cd23eee5d6f4ccb7fb6
Cesar250101 <Cesar250101@outlook.com>
2023-08-07 16:47:08 -0400

    Caf

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -11,6 +11,7 @@ import pdf417gen
 import xml.etree.ElementTree as ET
 from datetime import datetime
 from odoo.exceptions import AccessError, UserError, RedirectWarning, ValidationError, Warning
+from odoo.tools.float_utils import float_compare
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice.line'
@@ -185,6 +186,21 @@ class AccountInvoice(models.Model):
         if self.document_class_id.sii_code not in(46,110,112,43):
             return super(AccountInvoice, self).invoice_validate()        
 
+    @api.multi
+    def action_invoice_open(self):
+        # lots of duplicate calls to action_invoice_open, so we remove those already open
+        to_open_invoices = self.filtered(lambda inv: inv.state != 'open')
+        if to_open_invoices.filtered(lambda inv: not inv.partner_id):
+            raise UserError(_("The field Vendor is required, please complete it to validate the Vendor Bill."))
+        if to_open_invoices.filtered(lambda inv: inv.state != 'draft'):
+            raise UserError(_("Invoice must be in draft state in order to validate it."))
+        # if to_open_invoices.filtered(lambda inv: float_compare(inv.amount_total, 0.0, precision_rounding=inv.currency_id.rounding) == -1):
+        #     raise UserError(_("You cannot validate an invoice with a negative total amount. You should create a credit note instead."))
+        if to_open_invoices.filtered(lambda inv: not inv.account_id):
+            raise UserError(_('No account was found to create the invoice, be sure you have installed a chart of account.'))
+        to_open_invoices.action_date_assign()
+        to_open_invoices.action_move_create()
+        return to_open_invoices.invoice_validate()
 
 
     @api.model
